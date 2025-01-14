dd4e308bd36564c15b3dd2043722911e8d871d0d
Oleh Nahorniak <nagornyak68@gmail.com>
2025-01-14 21:08:29 +0200

    create persistence entity

---
+++ chat-api/src/main/java/org/aliezkan/chatapi/persistance/MessageEntity.java
@@ -0,0 +1,28 @@
+package org.aliezkan.chatapi.persistance;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.aliezkan.chatapi.domain.enums.EventType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String initiator;
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private EventType type;
+
+    private LocalDateTime dateTime;
+}
